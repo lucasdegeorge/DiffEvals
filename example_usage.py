cffb0c9e550815a3d1ceba93329543180b4d2217
Arijit Ghosh <arijit.ghosh@enpc.fr>
2025-07-30 04:21:38 +0200

    independednt hpsv2 calculation

--- example_usage.py
+++ example_usage.py
@@ -1,43 +1,57 @@
-"""Implementation of a simple example usage of the module."""
+# """Implementation of a simple example usage of the module."""
 
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from genmetrics import GenMetric, GenDataset
 
 
-def main():
-    ## Initialize the metric ##
-    metric = GenMetric(
-        metrics=["pickscore", "aesthetic_score", "hpsv2_score", "image_reward_score"]
-    )
-    metric = metric.to("cuda")  # Move to GPU if available
+def main(only_hpsv2_benchmark: bool = False):
+    if only_hpsv2_benchmark:
+        import hpsv2
 
-    ## Load a dataset ##
-    ## Here we assume that that images are generated and stored in a directory ##
-    dataset = GenDataset(
-        image_path="/data/ag/artifacts/neurips2025/partiprompts_images/cutmix_512/parti_long",
-        caption_path="/data/ag/artifacts/neurips2025/partiprompts_images/partiprompts.txt",
-    )
+        hpsv2.evaluate(
+            "/data/ag/artifacts/neurips2025/hpsv2_images/1024_crop_ft_laion_pop_400k/hpsv2.1/",
+            batch_size=40,
+            hps_version="v2.1",
+        )
+    else:
+        ## Initialize the metric ##
+        metric = GenMetric(
+            metrics=[
+                "pickscore",
+                "aesthetic_score",
+                "hpsv2_score",
+                "image_reward_score",
+            ]
+        )
+        metric = metric.to("cuda")  # Move to GPU if available
 
-    dl = DataLoader(
-        dataset,
-        batch_size=32,  # Adjust batch size as needed
-        shuffle=False,
-        num_workers=4,  # Adjust number of workers based on your system
-    )
-
-    ## Evaluate the dataset ##
-    for imgs, captions, _, _ in tqdm(dl):
-        imgs = imgs.to("cuda")
-        metric.update(
-            images=imgs,
-            captions=captions,
-            real=False,
+        ## Load a dataset ##
+        ## Here we assume that that images are generated and stored in a directory ##
+        dataset = GenDataset(
+            image_path="/data/ag/artifacts/neurips2025/partiprompts_images/1024_crop_imagenet_ft_400k/parti_long",
+            caption_path="/data/ag/artifacts/neurips2025/partiprompts_images/partiprompts.txt",
         )
 
-    scores = metric.compute()
-    print(scores)
+        dl = DataLoader(
+            dataset,
+            batch_size=32,  # Adjust batch size as needed
+            shuffle=False,
+            num_workers=4,  # Adjust number of workers based on your system
+        )
+
+        ## Evaluate the dataset ##
+        for imgs, captions, _, _ in tqdm(dl):
+            imgs = imgs.to("cuda")
+            metric.update(
+                images=imgs,
+                captions=captions,
+                real=False,
+            )
+
+        scores = metric.compute()
+        print(scores)
 
 
 if __name__ == "__main__":
-    main()
+    main(only_hpsv2_benchmark=True)
